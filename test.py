ce55c45383f588c21c2ef458f72885ba4579fa0a
watalo <watalo@163.com>
2020-08-06 23:10:46 +0800

    Day5 完成

--- test.py
+++ test.py
@@ -1,11 +1,29 @@
-x = input()
-lst = []
-try:
-    for i in range(1,5):
-        lst.append(x*i)
-    print(sum(map(int,lst)))
-except:
-    print('我只会算算数')
+
+import datetime as dt
+
+class Account:
+    def __init__(self):
+        self.statement = []
+        self.balance = 0
+        self.run()
+
+    def run(self):
+        x = input("存款按D/取款按W：")
+        tim = dt.datetime.now()
+        flag,val = x.split()
+        if flag =='D':
+            self.balance += int(val)
+            self.statement.append(('存入', val, tim.isoformat()))
+        elif flag == 'W':
+            self.balance -= int(val)
+            self.statement.append(('取出', val, tim.isoformat()))
+
+    def show(self):
+        for i in self.statement:
+            print(i)
+        print(self.balance)
 
 
-    
+acc = Account()
+acc.run()
+acc.show()
